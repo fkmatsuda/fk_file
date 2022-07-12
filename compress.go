6a0c667ae26d98f821bffb454416b20ea47f8818
Fábio Kenji Matsuda <fabio@fkmatsuda.dev>
2022-07-11 23:27:40 -0300

    feat: add zipfile which manages zip packages

--- compress.go
+++ compress.go
@@ -1,6 +1,7 @@
 package fkfile
 
 import (
+	"archive/zip"
 	"bufio"
 	"compress/gzip"
 	"io"
@@ -32,4 +33,41 @@ func CompressFile(src, dst string, removeSrc bool) error {
 
 	return w.Flush()
 
+}
+
+// ZipFile defines a zip file
+type ZipFile struct {
+	file   *os.File
+	writer *zip.Writer
+}
+
+// NewZipFile creates a new zip file
+func NewZipFile(file *os.File) (zipFile *ZipFile, err error) {
+	zipFile = &ZipFile{
+		file:   file,
+		writer: zip.NewWriter(file),
+	}
+	return
+}
+
+// Close closes the zip file
+func (zipFile *ZipFile) Close() error {
+	err := zipFile.writer.Close()
+	if err != nil {
+		return err
+	}
+	return zipFile.file.Close()
+}
+
+// AddEntry adds a file to the zip file
+func (zipFile *ZipFile) AddEntry(name string, reader io.Reader) error {
+	w, err := zipFile.writer.Create(name)
+	if err != nil {
+		return err
+	}
+	_, err = io.Copy(w, reader)
+	if err != nil {
+		return err
+	}
+	return nil
 }
